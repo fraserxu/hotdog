945b3f89108fe1888848ff0b6d3747481198a45a
Fraser Xu <xvfeng123@gmail.com>
2018-06-11 21:23:40 +1000

    first working version

--- src/main.ts
+++ src/main.ts
@@ -3,30 +3,44 @@ import * as mobilenet from '@tensorflow-models/mobilenet'
 const input: HTMLInputElement = document.querySelector('#input')
 const img: HTMLImageElement = document.querySelector('#img')
 const text: HTMLSpanElement = document.querySelector('#text')
+const loading: HTMLDivElement = document.querySelector('#loading')
+const cameraButton: HTMLButtonElement = document.querySelector('#camera')
+const captureButton: HTMLButtonElement = document.querySelector('#capture')
+const videoElement: HTMLVideoElement = document.querySelector('video')
+const canvasElement: HTMLCanvasElement = document.querySelector('#canvas')
+
+const width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth
 
 let model: mobilenet.MobileNet | undefined
+let mediaStream: MediaStream | undefined
 
-const load = async () => {
-  input.setAttribute('style', 'display: none;')
-  text.innerHTML = 'loading models...'
-  model = await mobilenet.load()
-  text.innerHTML = 'Model loaded. Now you can select an image to predict...'
-  input.setAttribute('style', 'display: block;')
+const hideElement = (element: HTMLElement) => {
+  element.setAttribute('style', 'display: none;')
 }
 
-const formatProbability = (probability: number) =>
-  `${(probability * 100).toFixed(2)}%`
+const showElement = (element: HTMLElement) => {
+  element.setAttribute('style', 'display: block;')
+}
+
+const load = async () => {
+  loading.setAttribute('style', 'display: none;')
+  loading.innerHTML = 'Loading models...'
+  model = await mobilenet.load()
+  loading.setAttribute('style', 'display: none;')
+}
+
+// load model
+load()
 
 const predict = async (img: HTMLImageElement) => {
   if (model) {
     const predictions = await model.classify(img)
-    const results = predictions.map(prediction => {
-      return `There are ${formatProbability(
-        prediction.probability
-      )} of chance that you select an image of ${prediction.className}.`
-    })
-
-    text.innerHTML = results.join('<br />')
+    const bestPrediction = predictions[0]
+    const classNames = bestPrediction.className.split(',')
+    text.innerHTML = `It's probably a ${classNames[0]} or ${classNames[1]}.`
   }
 }
 
@@ -36,7 +50,49 @@ const onSelectFiles = (event: Event) => {
   img.onload = () => predict(img)
 }
 
-input.addEventListener('change', onSelectFiles, false)
+const onCamera = async () => {
+  const constraints = {
+    audio: false,
+    video: { width },
+    facingMode: 'user'
+  }
+  mediaStream = await navigator.mediaDevices.getUserMedia(constraints)
+  hideElement(cameraButton)
+  videoElement.srcObject = mediaStream
+  videoElement.onloadedmetadata = () => {
+    videoElement.setAttribute('width', width.toString())
+    videoElement.play()
+  }
+  showElement(captureButton)
+}
 
-// load model
-load()
+const onCapture = () => {
+  hideElement(videoElement)
+  const context = canvasElement.getContext('2d')
+  canvasElement.width = videoElement.videoWidth
+  canvasElement.height = videoElement.videoHeight
+  context.drawImage(
+    videoElement,
+    0,
+    0,
+    videoElement.videoWidth,
+    videoElement.videoHeight
+  )
+
+  const imageData = canvasElement.toDataURL('image/png')
+  img.setAttribute('src', imageData)
+  img.onload = () => predict(img)
+
+  // stop media stream
+  if (mediaStream) {
+    mediaStream.getTracks().forEach((track: MediaStreamTrack) => {
+      track.stop()
+      mediaStream.removeTrack(track)
+    })
+  }
+  hideElement(captureButton)
+}
+
+cameraButton.addEventListener('click', onCamera, false)
+captureButton.addEventListener('click', onCapture, false)
+input.addEventListener('change', onSelectFiles, false)
