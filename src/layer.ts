1ed4484bfc1f6b92558c174e27e802da5b8a2f0f
Fraser Xu <xvfeng123@gmail.com>
2018-06-11 21:26:02 +1000

    add styles

---
+++ src/layer.ts
@@ -0,0 +1,43 @@
+import * as tf from '@tensorflow/tfjs'
+
+const model = tf.sequential()
+
+const hiddenLayer = tf.layers.dense({
+  units: 4,
+  inputShape: [2],
+  activation: 'sigmoid'
+})
+model.add(hiddenLayer)
+
+const outputLayer = tf.layers.dense({
+  units: 1,
+  activation: 'sigmoid'
+})
+model.add(outputLayer)
+
+const sgdOpt = tf.train.sgd(0.5)
+model.compile({
+  optimizer: sgdOpt,
+  loss: tf.losses.meanSquaredError
+})
+
+const xs = tf.tensor2d([[0, 0], [0.25, 0.25], [0.5, 0.5], [0.75, 0.75], [1, 1]])
+
+const ys = tf.tensor2d([[0], [0.25], [0.5], [0.75], [1]])
+
+const train = async (iteration: number) => {
+  for (let i = 0; i < iteration; i++) {
+    const response = await model.fit(xs, ys, { epochs: 10, shuffle: true })
+    console.log(response.history.loss[0])
+  }
+}
+
+const inputs = tf.tensor2d([[1, 1]])
+
+const main = async () => {
+  await train(500)
+  const predictions = await model.predict(inputs)
+  predictions.print()
+}
+
+main()
